bc0704818872920887d124d464311737cf77382a
siewcheng <siewcheng.tan@nielsen.com>
2020-08-29 19:26:40 +0200

    Fix entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 cd /var/www/akaunting
 php artisan migrate --no-interaction
 
-[[ "0${APP_HOSTNAME}" == "0"]] && APP_URL="localhost"
-sed "s/%APP_URL%/${APP_HOSTNAME}/" /tmp/akaunting.conf > /etc/nginx/conf.d/akaunting.conf
+[[ "0${APP_HOSTNAME}" == "0"]] && APP_HOSTNAME="localhost"
+sed "s/%APP_HOSTNAME%/${APP_HOSTNAME}/" /tmp/akaunting.conf > /etc/nginx/conf.d/akaunting.conf
 # exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 exec "$@"
